6a06f44b8485fb6af4ac1c0e7330bbd5d6a82d2b
saisrinivas18 <saisrinivasbandarupalli@gmail.com>
2017-10-31 16:52:36 +0000

    Test files

---
+++ src/test/java/com/mindtree/model/TodoTest.java
@@ -0,0 +1,15 @@
+package com.mindtree.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TodoTest {
+
+	@Test
+	public void test() {
+		String desc = "Sai Srinivas";
+		assertEquals(desc, "Sai Srinivas");
+	}
+
+}
